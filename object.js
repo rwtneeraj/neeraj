a1782089b4b20b56b5abec81404776f7a6994e96
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-04 17:52:43 +0530

    insert new object.js

---
+++ object.js
@@ -0,0 +1,13 @@
+// 16. Create an object to store information about a car (e.g., make, model, year). Add a method to display its details.\
+
+const carInformation = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2022,
+};
+
+const displayCarInformation = (carInformation) => {
+  console.log(carInformation);
+};
+
+displayCarInformation(carInformation);
