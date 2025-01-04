cd461c5a1b4000ef481d227484565c6e282cff8d
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-04 16:57:15 +0530

    added removeAllFalsyValue

--- arrays.js
+++ arrays.js
@@ -3,10 +3,17 @@
 const product = (numbers) => {
   return numbers.reduce((mul, num) => {
     mul = mul * num;
-    return mul
+    return mul;
   });
 };
 
-console.log(product([1, 2, 3, 4, 5]));  //120
-console.log(product([2,2,3]));         //12
-  
+console.log(product([1, 2, 3, 4, 5])); //120
+console.log(product([2, 2, 3])); //12
+
+// 7. Write a function to remove all falsy values from an array: `[0, 1, false, 2, '', 3, null, 4, undefined]`.
+
+const removeAllFalsyValue = (array) => array.filter((value) => value);
+
+console.log(removeAllFalsyValue([0, 1, false, 2, "", 3, null, 4, undefined]));
+console.log(removeAllFalsyValue([0, 1, false, 2, "", 3, 4, 5, NaN, 0]));
+console.log(removeAllFalsyValue(["NaN",NaN,"undefined",undefined]));
