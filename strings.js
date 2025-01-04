0bf478ee15c08a9b0dd379eaffd7f36bcfb60911
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-04 17:35:24 +0530

    added containsAlphanumeric

--- strings.js
+++ strings.js
@@ -1,10 +1,19 @@
 // 11. Write a function to reverse the words in the string: `"Hello World!"`.
 
 const reverseWordsInString = (strings) => {
-  const arrayOFWords = strings.split(" ")
-  return arrayOFWords.reverse().join(" ")
+  const arrayOFWords = strings.split(" ");
+  return arrayOFWords.reverse().join(" ");
 };
 
 console.log(reverseWordsInString("Hello World!"));
 console.log(reverseWordsInString("Hello javascript world"));
 
+// 12. Check if the string `"JavaScript"` contains only alphanumeric characters using regex.
+
+const containsAlphanumeric = (string) => {
+  const regex = /^[a-zA-Z0-9]+$/g;
+  return regex.test(string);
+};
+
+console.log(containsAlphanumeric("56kk22"));
+console.log(containsAlphanumeric("javaScript"));
